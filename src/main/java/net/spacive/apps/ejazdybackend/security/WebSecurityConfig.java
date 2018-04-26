ff585df1c98bcf3e449ab2076fe647e775691782
spacive <haluskajuraj0@gmail.com>
2018-04-26 22:39:04 +0200

    add cross origin anotations

--- src/main/java/net/spacive/apps/ejazdybackend/security/WebSecurityConfig.java
+++ src/main/java/net/spacive/apps/ejazdybackend/security/WebSecurityConfig.java
@@ -19,8 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().
-                authorizeRequests()
+        http.cors().and().authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
